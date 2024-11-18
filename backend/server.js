0dae4bf46e9505a63967da97cb37f5ff41455b55
Dhanada Panda <130206432+Dhanada-Panda@users.noreply.github.com>
2024-11-18 14:44:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
+import cors from 'cors';
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
@@ -18,6 +19,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
+
+app.use(cors({
+    origin: 'https://chatter-sphere-frontend.vercel.app', // Replace with your frontend's deployed URL
+    methods: 'GET,POST,PUT,DELETE', // Allowed methods
+    credentials: true, // Allow credentials (if needed)
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
